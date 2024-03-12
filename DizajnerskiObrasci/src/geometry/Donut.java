68a9f81a94a8503af27825ad51b6c3247b6748f8
Friday <Friday@Friday.local>
2024-03-12 16:02:37 +0100

    Donut transparency using java .awt classes

--- DizajnerskiObrasci/src/geometry/Donut.java
+++ DizajnerskiObrasci/src/geometry/Donut.java
@@ -1,14 +1,19 @@
 package geometry;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Area;
+import java.awt.geom.Ellipse2D;
 
 public class Donut extends Circle {
 
 	private int innerRadius;
 
 	public Donut() {
-		
+
 	}
 
 	public Donut(Point center, int radius, int innerRadius) {
@@ -53,32 +58,48 @@ public class Donut extends Circle {
 	}
 
 	public void fill(Graphics g) {
-		g.setColor(this.fillColor);
-		super.fill(g);
-		g.setColor(Color.WHITE);
-		g.fillOval(getCenter().getX() - this.innerRadius, getCenter().getY() - this.innerRadius, this.innerRadius * 2,
-				this.innerRadius * 2);
+		Graphics2D g2d = (Graphics2D) g.create();
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+		Ellipse2D outerCircle = new Ellipse2D.Double(getCenter().getX() - getRadius(), getCenter().getY() - getRadius(),
+				getRadius() * 2, getRadius() * 2);
+
+		Ellipse2D innerCircle = new Ellipse2D.Double(getCenter().getX() - innerRadius, getCenter().getY() - innerRadius,
+				innerRadius * 2, innerRadius * 2);
+
+		Area donutArea = new Area(outerCircle);
+		donutArea.subtract(new Area(innerCircle));
+		g2d.setColor(this.fillColor);
+		g2d.fill(donutArea);
+
+		g2d.dispose();
 	}
 
 	@Override
 	public void draw(Graphics g) {
 		super.draw(g);
+		Graphics2D g2d = (Graphics2D) g.create();
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		this.fill(g);
-		g.setColor(this.color);
-		g.drawOval(this.center.getX() - innerRadius, this.center.getY() - innerRadius, innerRadius + innerRadius,
-				innerRadius + innerRadius);
-
-	
+		g2d.setColor(this.color);
+		// Stroke
+		g2d.setStroke(new BasicStroke(2));
+		g2d.drawOval(this.center.getX() - getRadius(), this.center.getY() - getRadius(), getRadius() * 2,
+				getRadius() * 2);
+		g2d.drawOval(this.center.getX() - innerRadius, this.center.getY() - innerRadius, innerRadius * 2,
+				innerRadius * 2);
 
 		if (isSelected()) {
-			g.setColor(Color.BLUE);
-			g.drawRect(getCenter().getX() - 2, getCenter().getY() - 2, 4, 4);
-			g.drawRect(getCenter().getX() - innerRadius - 2, getCenter().getY() - 2, 4, 4);
-			g.drawRect(getCenter().getX() + innerRadius - 2, getCenter().getY() - 2, 4, 4);
-			g.drawRect(getCenter().getX() - 2, getCenter().getY() - innerRadius - 2, 4, 4);
-			g.drawRect(getCenter().getX() - 2, getCenter().getY() + innerRadius - 2, 4, 4);
-			g.setColor(Color.black);
+			g2d.setColor(Color.BLUE);
+			g2d.drawRect(getCenter().getX() - 2, getCenter().getY() - 2, 4, 4);
+			g2d.drawRect(getCenter().getX() - innerRadius - 2, getCenter().getY() - 2, 4, 4);
+			g2d.drawRect(getCenter().getX() + innerRadius - 2, getCenter().getY() - 2, 4, 4);
+			g2d.drawRect(getCenter().getX() - 2, getCenter().getY() - innerRadius - 2, 4, 4);
+			g2d.drawRect(getCenter().getX() - 2, getCenter().getY() + innerRadius - 2, 4, 4);
+			g2d.setColor(Color.BLACK);
 		}
+
+		g2d.dispose();
 	}
 
 	@Override
